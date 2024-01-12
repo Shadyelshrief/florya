55836aa06f917e6a0cd0fc17767f865a435a8442
Shady elshrief <shady.elshrief@gmail.com>
2024-01-12 13:55:00 +0200

    add contact details

---
+++ src/app/components/contact-us-details/contact-us-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-us-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-us-details.component.html',
+  styleUrl: './contact-us-details.component.scss'
+})
+export class ContactUsDetailsComponent {
+
+}
